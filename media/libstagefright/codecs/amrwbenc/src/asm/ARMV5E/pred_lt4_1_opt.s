d854b6da23bd71f7ae6724406360d1e185fb1ee8
Martin Storsjo <martin@martin.st>
2010-12-22 22:06:49 +0100

    amrwbenc: Prefix a few more symbols
    This avoids clashes between symbols in amrwbenc and amrwb, if they
    are linked into the same module.
    
    Change-Id: I1d26b35403ae1bc5a69fe4cf490abbc03286cb29

--- media/libstagefright/codecs/amrwbenc/src/asm/ARMV5E/pred_lt4_1_opt.s
+++ media/libstagefright/codecs/amrwbenc/src/asm/ARMV5E/pred_lt4_1_opt.s
@@ -31,7 +31,7 @@
 
          .section  .text
 	 .global   pred_lt4_asm
-	 .extern   inter4_2
+	 .extern   voAWB_inter4_2
 
 pred_lt4_asm:
 
@@ -453,7 +453,7 @@ pred_lt4_end:
          LDMFD     r13!, {r4 - r12, r15}
 
 Table:
-         .word       inter4_2-Table
+         .word       voAWB_inter4_2-Table
 	 @ENDFUNC
 	 .END
 
--- media/libstagefright/codecs/amrwbenc/src/asm/ARMV7/Filt_6k_7k_neon.s
+++ media/libstagefright/codecs/amrwbenc/src/asm/ARMV7/Filt_6k_7k_neon.s
@@ -27,7 +27,7 @@
 
           .section  .text
           .global   Filt_6k_7k_asm
-          .extern   fir_6k_7k
+          .extern   voAWB_fir_6k_7k
 
 Filt_6k_7k_asm:
 
@@ -223,7 +223,7 @@ Filt_6k_7k_end:
           LDMFD   		r13!, {r0 - r12, r15}
 
 Lable1:
-          .word   		fir_6k_7k-Lable1
+          .word   		voAWB_fir_6k_7k-Lable1
           @ENDFUNC
           .END
 
